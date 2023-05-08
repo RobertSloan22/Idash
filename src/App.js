329cbb236863f5e42b1646e269b1ff397feed613
Robert Sloan <89170263+RobertSloan22@users.noreply.github.com>
2023-05-08 06:40:32 -0500

    firstpush to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navigation from './components/Navbar';
+import Footer from './components/Footer';
+import JumbotronExamples from './components/JumbotronExamples';
+import JustHeroes from './components/JustHeroes';
+import Carousel from './components/Carousel';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import SideMenu from './components/SideMenu';
+import TextExtractor from './components/TextExtractor';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigation />
+      <JustHeroes />
+      <Carousel />
+      <SideMenu />
+      <JumbotronExamples />
+      <TextExtractor />
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
